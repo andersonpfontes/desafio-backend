8b8ff215748b637f07d72159f39138c35cd12c77
Anderson Fontes <anderson.fontes_ext@teladoc.com>
2023-07-18 23:51:51 -0300

    create interfaces for a pattern Strategy

---
+++ app/Repositories/Consumer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\{Models\Account, Models\Consumer as ConsumerModel, User};
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class Consumer implements ConsumerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function create(int $userId, string $username): array
+    {
+        try {
+            DB::beginTransaction();
+            $user = User::findOrFail($userId);
+            $user->username = $username;
+            $user->save();
+
+            $account = Account::create();
+            $consumer = new ConsumerModel();
+            $consumer->account_id = $account->id;
+            $consumer = $user->consumer()->save($consumer);
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+
+        return [
+            'id' => $consumer['id'],
+            'user_id' => $userId,
+            'username' => $username,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ConsumerController;
+use App\Http\Controllers\SellerController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('users')->group(function () {
+    Route::get('', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('', [UserController::class, 'store']);
+
+    Route::post('consumers', [ConsumerController::class, 'store']);
+    Route::post('sellers', [SellerController::class, 'store']);
+});
+
+Route::prefix('transactions')->group(function () {
+    Route::post('', [TransactionController::class, 'store']);
+    Route::get('/{id}', [TransactionController::class, 'show']);
+});
+
+//$router->group(['prefix' => 'transactions'], function () use ($router) {
+//
+//    // "External" service for authorization, for sake of simplicity is included with current API
+//    $router->post('authorize', function (Request $request) {
+//        $value = $request->get('value');
+//        if ($value >= 100.00) {
+//            return response('Transação não autorizada', 401);
+//        } else {
+//            return response('OK');
+//        }
+//    });
+//});
